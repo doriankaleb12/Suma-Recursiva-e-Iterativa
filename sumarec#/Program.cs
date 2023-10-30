2dfd579d58f9a12032547184910a50d61ccd5bb5
doriankaleb12 <146804704+doriankaleb12@users.noreply.github.com>
2023-10-29 19:51:49 -0600

    Add files via upload

---
+++ sumarec#/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese un número para sumar sus dígitos recursivamente:");
+        int num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("La suma recursiva de los dígitos de " + num + " es: " + SumaDigitosRecursiva(num));
+    }
+
+    static int SumaDigitosRecursiva(int num)
+    {
+        if (num == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return num % 10 + SumaDigitosRecursiva(num / 10);
+        }
+    }
+}
