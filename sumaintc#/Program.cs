21589f11dff8b675c6810783039dac5ff019efad
doriankaleb12 <146804704+doriankaleb12@users.noreply.github.com>
2023-10-29 19:50:58 -0600

    Add files via upload

---
+++ sumaintc#/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese un número para sumar sus dígitos iterativamente:");
+        int num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("La suma iterativa de los dígitos de " + num + " es: " + SumaDigitosIterativa(num));
+    }
+
+    static int SumaDigitosIterativa(int num)
+    {
+        int suma = 0;
+        while (num > 0)
+        {
+            suma += num % 10;
+            num /= 10;
+        }
+        return suma;
+    }
+}
